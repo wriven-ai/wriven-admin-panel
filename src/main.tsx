e9f23702e93c395e80eb5e6656f7d4dd365c47fb
anowarzz <anowarhosen444@gmail.com>
2026-06-30 11:25:00 +0600

    restructure features/ to pages/ with per-page components/ subfolder

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import '@fontsource-variable/manrope'
-import './styles/globals.css'
+import "@fontsource-variable/manrope";
+import "./styles/globals.css";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
-import { Toaster } from 'sonner'
-import { router } from './router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import { Toaster } from "sonner";
+import { router } from "./router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
     <Toaster position="bottom-right" richColors />
-  </StrictMode>,
-)
+  </StrictMode>
+);
